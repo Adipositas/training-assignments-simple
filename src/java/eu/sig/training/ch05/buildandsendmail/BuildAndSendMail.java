5aabe0b1570a12534ddf0972a2e95d3dc0ceb3a1
David Norsk <dno@dbigroup.com>
2020-01-29 14:53:48 +0100

    Encapsulated mail message, mailadress

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -1,18 +1,38 @@
 package eu.sig.training.ch05.buildandsendmail;
 
+public class Mail() {
+	public MailAddress mailAddress;
+	public MailBody mailBody;
+	public Mail(MailAddress mailAddress, MailBody mailBody) {
+		this.mailAddress = mailAddress;
+		this.mailBody = mailBody;
+	}
+}
+
+public class MailAddress() {
+	public String msgId;
+	public MailAddress(String firstName, String lastName, String devision) {
+		this.msgId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
+            + division.substring(0, 5) + ".compa.ny";
+	}
+}
+
+public class MailBody() {
+	public String subject;
+	public MailMessage message;
+	public MailBody(String subject, MailMessage message) {
+		this.subject = subject;
+		this.message = message;
+	}
+
+}
+
 public class BuildAndSendMail {
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
-        String message2, String message3) {
-        // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
-        // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(font,
-            message1 + message2 + message3);
-        // Send message
-        m.send(mId, subject, mMessage);
+    public void buildAndSendMail(MailMan m, MailAddress mailAddress, MailBody mailBody) {
+       Mail mail = Mail(mailAddress, mailBody);
+        
+        m.send(mail);
     }
     // end::buildAndSendMail[]
 
