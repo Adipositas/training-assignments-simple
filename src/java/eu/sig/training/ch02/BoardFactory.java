ac6d0a98b811358418d8c5874c51b38ebfd7926d
David Norsk <dno@dbigroup.com>
2020-01-29 11:13:11 +0100

    refactored

--- src/java/eu/sig/training/ch02/BoardFactory.java
+++ src/java/eu/sig/training/ch02/BoardFactory.java
@@ -26,9 +26,42 @@ public class BoardFactory {
     // end::createBoard[]
 }
 
+class BoardCreator {
+	private Square[][] grid;
+	private Board board;
+	private int width;
+	private int height;
+
+	BoardCreator(Square[][] grid]) {
+		assert grid != null;
+		this.grid = grid;
+		this.board = new Board(grid);
+		this.width = board.getWidth();
+		this.height = board.getHeight();
+	}
+
+	Board create() {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				for (Direction dir : Direction.values()) {
+					setLink(square, x, y);
+				}
+			}
+		}
+	}
+
+	private void setLink(Square square, Direction dir, int x, int y) {
+		int dirX = (width + x + dir.getDeltaX()) % width;
+        int dirY = (height + y + dir.getDeltaY()) % height;
+        Square neighbour = grid[dirX][dirY];
+        square.link(neighbour, dir);
+	}
+}
+
+
 class Board {
     @SuppressWarnings("unused")
-    public Board(Square[][] grid) {}
+    public Board(Square[][] grid) {	}
 
     public int getWidth() {
         return 0;
